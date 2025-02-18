0157194512fdecef5a3fbeab8bdf7f80f675eadc
nir <nrbnlulu@gmail.com>
2025-02-18 19:57:49 +0200

    refactor: update main.dart to initialize preferences and pass value to MyApp

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_preferences/flutter_preferences.dart';
+import 'package:flutter_preferences/flutter_preferences.dart' as flutter_preferences;
 
 Future<void> main() async {
-  await RustLib.init();
-  runApp(const MyApp());
+  await flutter_preferences.RustLib.init();
+  await flutter_preferences.init(appId: "my amazing app", author: "arthur the great");
+  await flutter_preferences.setString(key: "foo", value: "bar");
+  final val = (await flutter_preferences.getString(key: "foo"))!;
+
+  runApp(MyApp(val));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final String val;
+  const MyApp(this.val, {super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +21,7 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(title: const Text('flutter_rust_bridge quickstart')),
         body: Center(
           child: Text(
-              'Action: Call Rust `greet("Tom")`\nResult: `${greet(name: "Tom")}`'),
+              'Action: Call Rust `greet("Tom")`\nResult: `$val}`'),
         ),
       ),
     );
