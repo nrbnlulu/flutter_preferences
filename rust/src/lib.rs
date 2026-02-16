29d89787301f6a4cb55ac62c0a4072c3a5d5315f
Mortie <38497452+omortie@users.noreply.github.com>
2026-02-16 08:55:02 +0200

    built example in android
    * doc: https://cjycode.com/flutter_rust_bridge/guides/how-to/ndk-init
    used second method

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,3 +1,27 @@
 pub mod api;
 mod frb_generated;
 pub(crate) mod imp;
+
+#[cfg(target_os = "android")]
+mod init_android_context {
+    use jni::{objects::JClass, objects::JObject, objects::GlobalRef, JNIEnv};
+    use std::sync::OnceLock;
+    use std::ffi::c_void;
+
+    static CTX: OnceLock<GlobalRef> = OnceLock::new();
+
+    #[no_mangle]
+    pub extern "system" fn Java_com_example_flutter_1preferences_1example_FlutterPreferencesPlugin_initAndroid(
+        env: JNIEnv,
+        _class: JClass,
+        ctx: JObject,
+    ) {
+        let global_ref = env.new_global_ref(&ctx).expect("to make global reference");
+        let vm = env.get_java_vm().unwrap();
+        let vm = vm.get_java_vm_pointer() as *mut c_void;
+        unsafe {
+            ndk_context::initialize_android_context(vm, global_ref.as_obj().as_raw() as _);
+        }
+        CTX.get_or_init(|| global_ref);
+    }
+}
